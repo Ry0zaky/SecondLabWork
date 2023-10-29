51a6a2c652154f6a67fbed91931adfc2fadecb61
Ry0zaky <121617658+Ry0zaky@users.noreply.github.com>
2023-10-29 13:22:51 +0300

    Add files via upload

---
+++ Views/Components/LeftNavigationViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+public class LeftNavigationViewComponent : ViewComponent
+{
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var items = new[]
+        {
+            new { Controller = "Home", Action = "Index", Text = "Home" },
+            new { Controller = "Employee", Action = "Index", Text = "Employees" },
+            new { Controller = "Home", Action = "Privacy", Text = "Privacy" }
+        };
+
+        return View("Default", items);
+    }
+}
